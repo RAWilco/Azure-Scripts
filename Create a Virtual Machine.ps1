80cd6ef9d88e11996696f8a7e6009a8f26919aa7
Richard <wilcoxsonrichard@gmail.com>
2025-07-23 14:47:09 +0100

    10th commit

--- Create a Virtual Machine.ps1
+++ Create a Virtual Machine.ps1
@@ -17,12 +17,18 @@ $resourceGroup = Read-Host "Name the resource group for the VM (e.g. myResourceG
 $VMName = Read-Host "Enter a name for the VM (e.g. myVM)"
 $Location = Read-Host "Enter the Azure region for the VM (e.g. eastus, westus, etc.)"
 
-New-AzResourceGroup -Name 'myResourceGroup2' -Location 'eastus'
+# Prompt for VM login credentials
+$cred = Get-Credential -Message "Enter the username and password for the VM"
 
+# Create resource group
+New-AzResourceGroup -Name $resourceGroup -Location $Location
+
+# Create VM
 New-AzVm `
     -ResourceGroupName $resourceGroup `
     -Name $VMName `
     -Location $Location `
+    -Credential $cred `
     -Image 'MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest' `
     -VirtualNetworkName 'myVnet' `
     -SubnetName 'mySubnet' `
@@ -30,8 +36,9 @@ New-AzVm `
     -PublicIpAddressName 'myPublicIpAddress' `
     -OpenPorts 80,3389
 
-    Read-Host VM creation complete. Verify on Azure portal. Then press enter to remove the resource group and all resources created.
+Read-Host "VM creation complete. Verify on Azure portal. Then press Enter to remove the resource group and all resources created."
 
-    Remove-AzResourceGroup -Name $resourceGroup
+# Delete everything
+Remove-AzResourceGroup -Name $resourceGroup -Force
 
-    Read-Host "Resource group and all resources have been removed."
+Read-Host "Resource group and all resources have been removed."
